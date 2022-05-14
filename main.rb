a3950ff8fb84f70862ce41b4c2a1f15e7d5c63ed
Christian <christianvillere@gmail.com>
2022-05-14 11:13:12 -0700

    Add times loop to build_tree method

--- main.rb
+++ main.rb
@@ -6,8 +6,8 @@ class Node
 
   def initialize
     @data = data
-    @left_child = left_child
-    @right_child = right_child
+    @left_child = nil
+    @right_child = nil
   end
 
 end
@@ -18,13 +18,25 @@ class Tree
 
   def initialize
     @empty_array = []
-    @root = build_tree
+    @root = nil
   end
 
   def build_tree(input_array)
     #sort the list
+    sorted_array = input_array.sort
     #remove dups
+    uniq_array = sorted_array.uniq
     #make each number a new node object
+    n = uniq_array.length
+    n.times do |i|
+      new_node = Node.new
+      new_node.data = i
+      if self.root == nil
+        self.root = new_node.data
+      elsif
+
+
+    #turn it into a balance binary tree 
 
     #GOAL: return initial or level-0 root node
   end
